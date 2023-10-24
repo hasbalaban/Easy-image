9dc9cc21f226cefa6c7f1a2c3d567cdcc1032816
hasbalaban <hn73@outlook.com>
2023-10-24 23:52:00 +0300

    remove unnecessary code

--- app/src/main/java/com/example/easy_image/model/FavoriteDTO.kt
+++ app/src/main/java/com/example/easy_image/model/FavoriteDTO.kt
@@ -1,29 +1,8 @@
 package com.example.easy_image.model
-
-data class Favorites(
-    val imageList: ArrayList<FavoriteDTO>
-)
-
 data class FavoriteDTO(
     val id : Int,
     val imageUrl : String
 )
 
-object Favorite{
-    val favoriteImageList : Favorites = Favorites(ArrayList())
-
-    fun addOrRemoveFavorite(item : FavoriteDTO): Boolean {
-        return if (favoriteImageList.imageList.contains(item)) {
-            favoriteImageList.imageList.remove(item)
-            false
-        }
-        else {
-            favoriteImageList.imageList.add(item)
-            true
-        }
-
-    }
-
-}
 
 
