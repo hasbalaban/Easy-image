b69d820c61f5445b803b1790d5245abf9192d921
hasbalaban <hn73@outlook.com>
2023-10-30 22:47:00 +0300

    set repeat mode for videos

--- app/src/main/java/com/example/easy_image/view/VideoScreen.kt
+++ app/src/main/java/com/example/easy_image/view/VideoScreen.kt
@@ -1,30 +1,27 @@
 package com.example.easy_image.view
 
 import android.net.Uri
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
+import com.example.easy_image.R
 import com.example.easy_image.viewmodel.VideoViewModel
 import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.Player.REPEAT_MODE_ONE
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
@@ -56,7 +53,9 @@ fun VideoScreen(
         videos.value?.hits?.let {
             LazyColumn(modifier = Modifier.fillMaxSize()) {
                 items(it) {
-                    Column(modifier = Modifier.padding(top = 24.dp).fillMaxWidth()) {
+                    Column(modifier = Modifier
+                        .padding(top = 24.dp)
+                        .fillMaxWidth()) {
 
                         val videoURL =it.videos?.medium?.url
                         val source = ProgressiveMediaSource.Factory(dataSourceFactory)
@@ -68,7 +67,9 @@ fun VideoScreen(
                                 repeatMode = REPEAT_MODE_ONE
                             }
                         }
-                        AndroidView(modifier = Modifier.fillMaxWidth().height(160.dp), factory = { context ->
+                        AndroidView(modifier = Modifier
+                            .fillMaxWidth()
+                            .height(240.dp), factory = { context ->
 
                             PlayerView(context).apply {
                                 player = mExoPlayer
