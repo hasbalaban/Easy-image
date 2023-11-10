e0cfec732824552254156d5aad8e9450b156ca4c
hasbalaban <hn73@outlook.com>
2023-11-10 23:21:42 +0300

    object convert to data class

--- app/src/main/java/com/example/easy_image/utils/Constants.kt
+++ app/src/main/java/com/example/easy_image/utils/Constants.kt
@@ -2,11 +2,11 @@ package com.example.easy_image.utils
 
 
 sealed class NavigationDirections(val route: String) {
-    object HomeScreen : NavigationDirections("homeScreen")
-    object Favorite : NavigationDirections("favoriteScreen")
-    object Video : NavigationDirections("videoScreen")
-    object SearchScreen : NavigationDirections("SearchScreen")
-    object DetailScreen: NavigationDirections("DetailScreen?imageUrl={imageUrl}&hash={hash}") {
+    data object HomeScreen : NavigationDirections("homeScreen")
+    data object Favorite : NavigationDirections("favoriteScreen")
+    data object Video : NavigationDirections("videoScreen")
+    data object SearchScreen : NavigationDirections("SearchScreen")
+    data object DetailScreen: NavigationDirections("DetailScreen?imageUrl={imageUrl}&hash={hash}") {
         fun createRoute(imageUrl: String) = "DetailScreen?imageUrl=$imageUrl"
     }
 
