894b5524bfda1b9b36cfc9ba7aa3719e9fcf4ba4
hasbalaban <hn73@outlook.com>
2023-11-10 23:13:15 +0300

    update gradle

--- mediapi/src/main/java/com/easyImage/mediapi/service/VideoService.kt
+++ mediapi/src/main/java/com/easyImage/mediapi/service/VideoService.kt
@@ -1,8 +1,6 @@
 package com.easyImage.mediapi.service
 
 import com.easyImage.mediapi.model.VideoResponse
-import com.easyImage.mediapi.utils.WrapResponse
-import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
