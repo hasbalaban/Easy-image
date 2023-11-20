fe0be706eea3f525071ba8410ea2d6e64f7d9f03
hasbalaban <hn73@outlook.com>
2023-11-20 22:47:36 +0300

    create fake repositories

--- mediapi/src/main/java/com/easyImage/mediapi/repository/video/VideoRepositoryImpl.kt
+++ mediapi/src/main/java/com/easyImage/mediapi/repository/video/VideoRepositoryImpl.kt
@@ -1,31 +1,75 @@
 package com.easyImage.mediapi.repository.video
 
+import com.easyImage.mediapi.model.VideoItemDTO
 import com.easyImage.mediapi.model.VideoResponse
 import com.easyImage.mediapi.utils.Resource
 import com.easyImage.mediapi.utils.handleResponse
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-class VideoRepositoryImpl @Inject constructor(
-    private val localVideoDataSource: LocalVideoDataSource,
+open class VideoRepositoryImpl @Inject constructor(
     private val remoteVideoDataSource: RemoteVideoDataSource,
-) {
+) : VideoRepositoryOperations1 {
+    override suspend fun getVideos(
+        time: Long,
+        key : String,
+        query : String,
+        page : Int
+    ): Flow<Resource<List<VideoItemDTO>?>> = flow {
+            val videos = remoteVideoDataSource.getVideos(
+                key = "39342921-c040c554a9e966b3202b73519",
+                query = query,
+                page = page
+            )
+
+            videos.handleResponse().collect{
+                when(it.status){
+                    Resource.Status.SUCCESS -> {
+                        val videoItemDto = it.data?.hits?.mapIndexed { index, videoItem ->
+
+                            VideoItemDTO(
+                                id = videoItem.id,
+                                videoPreviewUrl = videoItem.videos?.small?.url ?: "",
+                                videoUrl = videoItem.videos?.large?.url ?: "",
+                                isVideoPlaying = true,
+                                isMusicOpen = true,
+                                videoTag =videoItem.tags ?: ""
+                            )
+                        }
+
+
+                        emit(Resource.success(videoItemDto))
+                    }
+                    Resource.Status.ERROR -> emit(Resource.error("error", null))
+                    Resource.Status.LOADING -> emit(Resource.loading(null))
+                    Resource.Status.RESET ->emit( Resource.reset())
+                }
+            }
+
+
+        }
+
+
+}
+
+interface VideoRepositoryOperations{
     suspend fun getVideos(
         time: Long,
         key : String,
         query : String,
         page : Int
 
-    ): Flow<Resource<VideoResponse?>> {
-        return if (time > 10) remoteVideoDataSource.getVideos(
-                key = key,
-                query = query,
-                page = page
-        ).handleResponse()
-        else localVideoDataSource.getVideos(
-            key = key,
-            query = query,
-            page = page
-        ).handleResponse()
-    }
+    ): Flow<Resource<VideoResponse?>>
+
+}
+interface VideoRepositoryOperations1{
+    suspend fun getVideos(
+        time: Long,
+        key : String,
+        query : String,
+        page : Int
+
+    ): Flow<Resource<List<VideoItemDTO>?>>
+
 }
