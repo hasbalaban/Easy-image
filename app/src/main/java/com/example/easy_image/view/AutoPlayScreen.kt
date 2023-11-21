fa0f9ed805d692577fff3cd94bbb93748c4e5494
hasbalaban <hn73@outlook.com>
2023-11-21 20:32:00 +0300

    change text modifer - alignment - padding

--- app/src/main/java/com/example/easy_image/view/AutoPlayScreen.kt
+++ app/src/main/java/com/example/easy_image/view/AutoPlayScreen.kt
@@ -12,7 +12,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -26,6 +28,7 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.easy_image.utils.ExoPlayerManager
@@ -96,7 +99,7 @@ fun AutoPlayScreen (
                                     }
                                 }
                             }
-                            Text(text = it.videoTag, color = Color.White)
+                            Text(modifier = Modifier.fillMaxWidth().padding(end = 12.dp), text = it.videoTag, color = Color.White, textAlign = TextAlign.End)
                         }
 
 
